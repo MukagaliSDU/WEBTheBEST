a5144f4b8de4e1188cd09813f31e784cd4aed12e
Dias Lesbay <210103095@stu.sdu.edu.kz>
2022-12-18 01:16:04 +0600

    all main changes

--- animation.js
+++ animation.js
@@ -5,6 +5,8 @@ function openNav() {
     document.body.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
     document.getElementsByClassName("text_title")[0].style.color = "white";
     document.getElementsByClassName("text_reklame")[0].style.color = "white";
+    document.getElementsByTagName("header")[0].style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+
   }
   
   function closeNav() {
@@ -15,6 +17,7 @@ function openNav() {
     document.getElementsByClassName("text_reklame")[0].style.color = "black";
     document.getElementById("mySidenav").style.width = "0";
     document.getElementsByClassName("section")[0].style.marginLeft = "1%";
+    document.getElementsByTagName("header")[0].style.backgroundColor = "white";
   }
 
   
